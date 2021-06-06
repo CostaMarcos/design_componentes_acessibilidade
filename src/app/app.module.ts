55a22f7700bee39f0cfbfc9a9194a1378e70ca3c
Marcos Vinicius <marcosvinicius.mecatronica@gmail.com>
2021-06-06 19:08:01 -0400

    criando um form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    YesNoButtonGroupModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
